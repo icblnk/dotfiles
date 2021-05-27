e5b993fd83da6e63f73c50074507f80cd5fa58f7
icblnk <john.iceblink@gmail.com>
2021-05-27 21:54:18 +0200

    Switch to i3-gaps

---
+++ scripts/run_polybar.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+source ~/.bash_functions.sh
+
+killall polybar 2> /dev/null
+
+primary_monitor=$(get_primary_monitor)
+
+for monitor in $(get_connected_monitors);
+do
+    if [ "$monitor" = "$primary_monitor" ];
+    then
+        MONITOR=$monitor polybar main &
+    else
+        MONITOR=$monitor polybar aux &
+    fi
+done
