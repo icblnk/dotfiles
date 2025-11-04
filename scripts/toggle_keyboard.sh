b203938685357274151d2f716fe7f6dbc8183be3
Ievgen Glinchuk <john.iceblink@gmail.com>
2025-11-04 22:58:28 +0100

    Fix swedish layout for sway

--- scripts/toggle_keyboard.sh
+++ scripts/toggle_keyboard.sh
@@ -1,26 +1,16 @@
 #!/usr/bin/sh
 
-KB_FLAG="/tmp/___keyboard_layout.flag"
-
 enable_se() {
-    echo "ON" > "$KB_FLAG"
-    setxkbmap -model pc105 -layout se,ua -option grp:alt_shift_toggle
+    swaymsg 'input "type:keyboard" xkb_layout "se,ua"'
 }
 
 disable_se() {
-    echo "OFF" > "$KB_FLAG"
-    setxkbmap -model pc105 -layout us,ua -option grp:alt_shift_toggle
+    swaymsg 'input "type:keyboard" xkb_layout "us,ua"'
 }
 
-if [[ -f "$KB_FLAG" ]];
+if [[ $(swaymsg -t get_inputs | jq -r '[.[] | select(.type == "keyboard")][0] | .xkb_layout_names[0]') =~ "English" ]];
 then
-    grep "ON" "$KB_FLAG" > /dev/null
-    if [[ "$?" -ne 0 ]];
-    then
-        enable_se
-    else
-        disable_se
-    fi
-else
     enable_se
+else
+    disable_se
 fi
